4e49abf3d0b79933cd4ce57ad1d4c4b2d4050a84
DuBento <duarte.bento@tecnico.ulisboa.pt>
2023-06-22 09:43:27 +0100

    feat: OZ sample initial contracts enable optimizer to necessarly reduce contract size #2

---
+++ blockchain/contracts/DAO/GovernorToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "../OpenZeppelin/token/ERC20/ERC20.sol";
+import "../OpenZeppelin/token/ERC20/extensions/ERC20Permit.sol";
+import "../OpenZeppelin/token/ERC20/extensions/ERC20Votes.sol";
+
+contract GovernorToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20("GovernorToken", "GTK") ERC20Permit("GovernorToken") {}
+
+    // The functions below are overrides required by Solidity.
+
+    function nonces(
+        address owner
+    ) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, amount);
+    }
+}
--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -2,7 +2,15 @@ import "@nomicfoundation/hardhat-toolbox";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   typechain: {
     outDir: "./artifacts/frontend-artifacts",
     target: "ethers-v5",
