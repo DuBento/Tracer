5c6bc6f070169cdda59eb71f14dbc9bec156958c
DuBento <duarte.bento@tecnico.ulisboa.pt>
2023-06-28 14:33:28 +0100

    feat: add governor settings extension

--- blockchain/deploy/03_governor.ts
+++ blockchain/deploy/03_governor.ts
@@ -1,6 +1,7 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
+import { VOTING_DELAY, VOTING_PERIOD, VOTING_QUORUM } from "../properties";
 import { padCenter, scriptName, storeContractAddress } from "../scripts/utils";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
@@ -16,7 +17,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const governorContract = await deploy("GovernorContract", {
     from: deployer,
-    args: [governorToken.address, governorTimelock.address],
+    args: [
+      governorToken.address,
+      governorTimelock.address,
+      VOTING_DELAY,
+      VOTING_PERIOD,
+      VOTING_QUORUM,
+    ],
     log: true,
     // TODO verify if live on network
   });
