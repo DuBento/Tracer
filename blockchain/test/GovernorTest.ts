46ac94c84beea2ef5a23fd3704cb6f4bbb958d4e
DuBento <duarte.bento@tecnico.ulisboa.pt>
2023-07-19 13:20:38 +0100

    test: new member vote

--- blockchain/test/GovernorTest.ts
+++ blockchain/test/GovernorTest.ts
@@ -15,6 +15,10 @@ import { newSupplychainContractViaGovernance } from "../lib/newSupplychainContra
 import { propose } from "../lib/propose";
 import * as utils from "../lib/utils";
 import { vote } from "../lib/vote";
+import {
+  USER_REGISTRY_UPDATE_MEMBER_DESCRIPTION,
+  USER_REGISTRY_UPDATE_MEMBER_STATE_METHOD,
+} from "../properties";
 import { createNewBatch } from "./SupplyChainTest";
 import {
   ACTOR_INFO_URI,
@@ -44,6 +48,14 @@ describe("Governor", function () {
       "SupplychainFactory"
     );
     userRegistry = await utils.getContract<UserRegistry>("UserRegistry");
+
+    console.log(`#####
+    GovernorContract: ${await governorContract.getAddress()}
+    GovernorToken: ${await governorToken.getAddress()}
+    GovernorTimelock: ${await governorTimelock.getAddress()}
+    SupplychainFactory: ${await supplychainFactory.getAddress()}
+    UserRegistry: ${await userRegistry.getAddress()}
+    #####`);
   });
 
   it("Succesfully deploys and Timelock is the correct owner", async function () {
@@ -51,6 +63,90 @@ describe("Governor", function () {
     expect(await supplychainFactory.owner()).to.equal(
       await governorTimelock.getAddress()
     );
+  });
+
+  describe("User Registry", function () {
+    it("Add member succesfully", async function () {
+      const { supplychainManager } = await getNamedAccounts();
+      const { member } = await newMemberViaGovernance(supplychainManager);
+      expect(member.addr).to.equal(supplychainManager);
+    });
+
+    it("Add actor succesfully, with unregistered no privilege account", async function () {
+      const { actor1 } = await getNamedAccounts();
+      const userRegistryAsActor1 = userRegistry.connect(
+        await ethers.getSigner(actor1)
+      );
+      await userRegistryAsActor1.addActor(actor1, ACTOR_NAME, ACTOR_INFO_URI);
+
+      const resActor = await userRegistry.actors(actor1);
+      expect(resActor.addr).to.equal(actor1);
+      expect(resActor.name).to.equal(ACTOR_NAME);
+      expect(resActor.infoURI).to.equal(ACTOR_INFO_URI);
+      expect(resActor.state).to.equal(
+        await userRegistry.CONFORMITY_STATE_FUNCTIONING()
+      );
+    });
+
+    xit("Add supplychain contract to actor", async function () {
+      // TODO: not trival due to contract ownership
+    });
+
+    it("Update actor with different account, unsuccesfully", async function () {
+      const { actor1 } = await getNamedAccounts();
+      const userRegistryAsActor1 = userRegistry.connect(
+        await ethers.getSigner(actor1)
+      );
+      await userRegistryAsActor1.addActor(actor1, ACTOR_NAME, ACTOR_INFO_URI);
+
+      const { actor2 } = await getNamedAccounts();
+      const userRegistryAsActor2 = userRegistry.connect(
+        await ethers.getSigner(actor2)
+      );
+      await expect(
+        userRegistryAsActor2.updateActor(actor1, ACTOR_NAME, ACTOR_INFO_URI)
+      ).to.be.revertedWithCustomError(
+        userRegistry,
+        "TransactionNotFromOriginalActorAddress"
+      );
+    });
+  });
+
+  describe("Proposals", function () {
+    const encodeFunctionCall = async (addr: string, state: string) =>
+      await utils.encodeFunctionCall(
+        "UserRegistry",
+        USER_REGISTRY_UPDATE_MEMBER_STATE_METHOD,
+        [addr, state]
+      );
+
+    it("Vote proposal with new member succesfully", async function () {
+      const { actor1 } = await getNamedAccounts();
+      await newMemberViaGovernance(actor1);
+
+      const newState =
+        await userRegistry.CONFORMITY_STATE_CORRECTIVE_MEASURE_NEEDED();
+      const encodedCall = await encodeFunctionCall(actor1, newState.toString());
+
+      const proposalId = await propose(
+        await utils.getContractAddress("UserRegistry"),
+        encodedCall,
+        USER_REGISTRY_UPDATE_MEMBER_DESCRIPTION
+      );
+
+      let state = await vote(
+        proposalId.toString(),
+        1,
+        PROPOSAL_VOTE_DESCRIPTION,
+        actor1
+      );
+
+      expect(state).to.equal(4); // 4 = Succeeded
+    });
+
+    it("Propose with registered actor succesfully", async function () {});
+    it("Propose with unregistered actor, unsuccesfully", async function () {});
+    it("Vote with actor, unsuccesfully", async function () {});
   });
 
   describe("Supplychain factory", function () {
