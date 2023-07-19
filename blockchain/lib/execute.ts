99a161eb73029e9b6bfa1f1478c5d7360ec141fd
DuBento <duarte.bento@tecnico.ulisboa.pt>
2023-07-19 17:54:20 +0100

    feat: governor without OZ timelock. Default governor executer #11

--- blockchain/lib/execute.ts
+++ blockchain/lib/execute.ts
@@ -1,7 +1,6 @@
-import { ethers, network } from "hardhat";
+import { ethers } from "hardhat";
 import { GovernorContract } from "../artifacts-frontend/typechain";
 import * as utils from "../lib/utils";
-import { DEVELOPMENT_CHAINS, MIN_DELAY } from "../properties";
 
 export async function execute(
   proposalTarget: string,
@@ -15,20 +14,6 @@ export async function execute(
   const governor = await utils.getContract<GovernorContract>(
     "GovernorContract"
   );
-
-  console.log("Queueing...");
-  const queueTx = await governor.queue(
-    [proposalTarget],
-    [0],
-    [encodedCall],
-    descriptionHash
-  );
-  await queueTx.wait();
-
-  if (DEVELOPMENT_CHAINS.includes(network.name)) {
-    await utils.increaseBlocks(1);
-    await utils.increaseTime(MIN_DELAY + 1);
-  }
 
   console.log("Executing...");
   // this will fail on a testnet because you need to wait for the MIN_DELAY!
