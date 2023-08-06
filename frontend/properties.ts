221d4b014ac57abb65518c75f0f39d7a7ab8d962
DuBento <duarte.bento@tecnico.ulisboa.pt>
2023-08-06 09:08:01 +0100

    feat: smaller qrcode - shorten path name - decrease error level and logo size - base64url the batch id #13

--- frontend/properties.ts
+++ frontend/properties.ts
@@ -1,4 +1,5 @@
 export const GS1_DATA_LINK_GTIN_PREFIX = "01";
 export const GS1_DATA_LINK_BATCH_PREFIX = "10";
 
-export const CLIENT_VIEW_PAGE_LOCATION = "view";
+export const CLIENT_VIEW_PAGE_LOCATION = "v";
+export const QR_CODE_PROTOCOL = "http"; // TODO change to https per the GS1 DL standard
