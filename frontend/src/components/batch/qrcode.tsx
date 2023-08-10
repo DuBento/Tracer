ec802b1f5e63ad211219caf16b3db69a94ddd9b9
DuBento <duarte.bento@tecnico.ulisboa.pt>
2023-08-10 09:54:24 +0100

    feat: downloadable svg qrcode migrate: replaced qrcode.react dependency for qrcode #13

--- frontend/src/components/batch/qrcode.tsx
+++ frontend/src/components/batch/qrcode.tsx
@@ -8,16 +8,16 @@ import {
   QR_CODE_PROTOCOL,
 } from "@/properties";
 import base64url from "base64url";
-import { QRCodeSVG } from "qrcode.react";
 import { useContext, useState } from "react";
+import LogoQrCode from "../common/logoQrCode";
 
 const QRCode = ({}) => {
   const { batch } = useContext(BatchContext);
 
   const [gtin, setGtin] = useState<string>("");
-  const [qrcode, setQrcode] = useState<JSX.Element>();
+  const [qrCodeElement, setQrCodeElement] = useState<JSX.Element>();
 
-  const generateQrcode = () => {
+  const generateQrcode = async () => {
     console.log({ batch, gtin });
     if (!batch?.id) return;
 
@@ -34,46 +34,59 @@ const QRCode = ({}) => {
       `${QR_CODE_PROTOCOL}://${window.location.host}`
     ).toString();
 
-    console.log({ url }, batch?.id);
-
-    setQrcode(
-      <QRCodeSVG
-        value={url}
-        bgColor={"#ffffff"}
-        fgColor={"#000000"}
-        level={"M"}
-        includeMargin={true}
-        imageSettings={{
-          src: "/images/logo.svg",
-          height: 24,
-          width: 24,
-          excavate: true,
-        }}
-        className="rounded-md"
-      />
+    setQrCodeElement(
+      <div
+        id="qrcode-created-container"
+        className="rounded-md h-full aspect-square overflow-hidden"
+      >
+        <LogoQrCode url={url} imageSize={24} />
+      </div>
     );
+  };
+
+  const downloadQrcode = () => {
+    const newQrcode = document.getElementById("qrcode-created-container");
+    const blob = new Blob([newQrcode!.innerHTML], { type: "image/svg+xml" });
+
+    const element = document.createElement("a");
+    element.download = "qrcode.svg";
+    element.href = window.URL.createObjectURL(blob);
+    element.click();
+    element.remove();
   };
 
   if (!batch) return null;
 
-  if (qrcode)
+  if (qrCodeElement)
     return (
-      <div className="flex flex-row items-center h-full">
-        {qrcode}
+      <div className="flex flex-row items-center w-full h-full py-4">
+        {qrCodeElement}
         <div className="flex flex-col justify-center ml-4">
           <button
-            className="my-2 px-4 py-2 rounded-md bg-red-300 hover:bg-red-400 text-white font-semibold"
-            onClick={() => setQrcode(undefined)}
+            className="my-2 p-2 h-12 w-12 rounded-full bg-red-300 hover:bg-red-400 font-semibold"
+            onClick={() => setQrCodeElement(undefined)}
           >
-            Back
+            {/* close icon */}
+            <svg
+              className="h-full w-full"
+              viewBox="0 -960 960 960"
+              fill="currentColor"
+            >
+              <path d="M480-416 281-218q-13 13-32 13.5T218-218q-14-13-14-31.5t14-32.5l199-198-199-199q-14-13-14-31.5t14-32.5q12-13 31-12.5t32 13.5l199 198 199-198q13-13 32-13.5t31 12.5q14 14 14 32.5T743-679L544-480l199 198q13 14 13 32.5T742-218q-12 14-31 13.5T679-218L480-416Z" />
+            </svg>
           </button>
           <button
-            className="my-2 px-4 py-2 rounded-md bg-blue-500 hover:bg-blue-600 text-white font-semibold"
-            onClick={() => {
-              // TODO Add download logic here
-            }}
+            className="my-2 p-2 h-12 w-12 rounded-full bg-blue-500 hover:bg-blue-600 text-white font-semibold"
+            onClick={downloadQrcode}
           >
-            Download
+            {/* download icon */}
+            <svg
+              className="h-full w-full"
+              viewBox="0 -960 960 960"
+              fill="currentColor"
+            >
+              <path d="M480.256-342q-8.399 0-17.849-3.773Q452.957-349.545 447-356L288-516q-14-12.364-13.708-31.208.291-18.844 14.317-32.363Q301.4-593.154 321-592.577 340.6-592 354-579l81 81v-278q0-19.65 12.86-32.825Q460.719-822 480.36-822 500-822 513-808.825T526-776v278l82-81q12.667-13 30.748-13.657 18.082-.657 32.411 12.577Q684-567 684-547.682q0 19.318-13 33.682L513-356q-5.81 6.455-15.221 10.227Q488.368-342 480.256-342ZM230-139q-37.175 0-64.087-26.913Q139-192.825 139-230v-98q0-18.375 12.86-31.688Q164.719-373 184.36-373 204-373 217-359.688q13 13.313 13 31.688v98h500v-98q0-18.375 13.56-31.688Q757.119-373 775.772-373q20.053 0 33.14 13.312Q822-346.375 822-328v98q0 37.175-27.206 64.087Q767.588-139 730-139H230Z" />
+            </svg>
           </button>
         </div>
       </div>
