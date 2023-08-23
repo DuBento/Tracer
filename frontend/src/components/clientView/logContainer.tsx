a45673d142ebd05646c017c9f8c0d1742ba312c8
DuBento <duarte.bento@tecnico.ulisboa.pt>
2023-08-23 11:41:21 +0100

    fix: cleanup collapsable header and migrate to tailwind

--- frontend/src/components/clientView/logContainer.tsx
+++ frontend/src/components/clientView/logContainer.tsx
@@ -11,10 +11,8 @@ export default function LogContainer(props: Props) {
     if (transitioning) return;
 
     const container = document.getElementById("cv-log-container");
-    console.log(container?.scrollTop);
     var description = document.getElementById("cv-header-description");
 
-    // var wrapper = document.getElementById("cv-measuringWrapper");
     window.requestAnimationFrame(() => {
       if (container?.scrollTop! > 50 && description?.style.maxHeight != "0px") {
         transitioning = true;
@@ -29,22 +27,11 @@ export default function LogContainer(props: Props) {
     });
   }
 
-  function resizeOnClick() {
-    var description = document.getElementById("cv-header-description");
-
-    if (description?.style.maxHeight == "0px") {
-      description!.style.maxHeight = description.scrollHeight + "px";
-    } else {
-      description!.style.maxHeight = "0px";
-    }
-  }
-
   return (
     <div
       id="cv-log-container"
       className="relative max-h-screen overflow-y-scroll"
       onScroll={(e) => resizeHeader(e)}
-      //   onClick={() => resizeOnClick()}
       onTransitionEnd={() => (transitioning = false)}
     >
       {props.children}
