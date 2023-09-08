3674e3e16f9c680c2640327f324f26f68cfbfb17
DuBento <duarte.bento@tecnico.ulisboa.pt>
2023-09-08 19:44:08 +0100

    feat: mobile homepage

---
+++ frontend/src/app/dao/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <a href="/">
+      <div className="flex h-screen items-center justify-center bg-redwood-800">
+        <div className="rounded-lg bg-white p-8 shadow-md">
+          <h1 className="mb-4 text-4xl font-semibold">Under Construction</h1>
+          <p className="text-gray-600">
+            This website is currently under construction.
+          </p>
+          <p className="text-gray-600">Please check back later.</p>
+        </div>
+      </div>
+    </a>
+  );
+}
